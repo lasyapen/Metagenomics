6af7ee39f92a7149f1573bec80fad0ff7cd95da9
lasyapenumarthi <89758944+lasyapen@users.noreply.github.com>
2026-02-09 12:32:16 -0500

    Create humann.sh

---
+++ humann.sh
@@ -0,0 +1,28 @@
+ml Mamba
+
+mamba create -p ~/envs/mpa5 -y python=3.10
+source ~/envs/mpa5/bin/activate
+
+conda config --add channels defaults
+conda config --add channels bioconda
+conda config --add channels conda-forge
+conda config --add channels biobakery
+
+~/envs/mpa5/bin/pip install humann --no-binary :all: --ignore-installed
+conda install -c bioconda bowtie2 metaphlan -p ~/envs/mpa5
+
+export PATH=~/.conda/envs/mpa4/bin:~/envs/mpa5/bin:$PATH
+humann_config --update database_folders nucleotide humann_databases/chocophlan
+humann_databases --download chocophlan full humann_databases --update-config no
+humann_databases --download uniref uniref90_diamond humann_databases --update-config no
+
+mkdir -p humann_output
+
+for f in *_R1_cleaned.fastq.gz; do
+    sample=$(basename "$f" _R1_cleaned.fastq.gz)
+    humann --input "$f" --output "humann_output/${sample}" --threads 8 \
+       --nucleotide-database humann_databases/chocophlan \
+       --protein-database humann_databases/uniref \
+       --metaphlan-options "--bowtie2db humann_databases/metaphlan"
+done
+
